a55d187d2366e34f75787e3857a17676867f1cc9
hihira <hihiranomail@gmail.com>
2015-09-22 18:09:23 +0900

    GAE版初版

--- hatebu-web-hook.php
+++ hatebu-web-hook.php
@@ -1,4 +1,7 @@
 <?php
+include './Secret.php';
+define('URL_QIITA_API', 'https://qiita.com/api/v2/items/');
+
 $url = parse_url($_POST['url']);
 if (strpos($url['host'], 'qiita.com') === false) {
     error_log("not qiita.com");
@@ -8,20 +11,38 @@ if (strpos($url['host'], 'qiita.com') === false) {
 $itemId = explode('/', $url['path'])[3];
 error_log('stocking item id: '.$itemId);
 
-$ch = curl_init();
-curl_setopt_array($ch, [
-    CURLOPT_PUT => true,
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_URL => 'https://qiita.com/api/v2/items/'.$itemId.'/stock',
-    CURLOPT_HTTPHEADER => [
-        'Content-type: application/json',
-        'Authorization: Bearer '.getenv('ACCESS_TOKEN_QIITA'),
-    ],
-]);
-$response = curl_exec($ch);
+$url = URL_QIITA_API.$itemId.'/stock';
+$qiitaAccessToken = Secret::getQiita();
 
-if ($response !== false) {
-    exit;
+if (function_exists('curl_init')) {
+    $ch = curl_init();
+    curl_setopt_array($ch, [
+        CURLOPT_PUT => true,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_URL => $url,
+        CURLOPT_HTTPHEADER => [
+            'Content-type: application/json',
+            'Authorization: Bearer '.$qiitaAccessToken,
+        ],
+    ]);
+    $response = curl_exec($ch);
+    if ($response === false) {
+        error_log('Qiita API response error: curl failed. '.curl_error($ch));
+    }
+} else {
+    $context = [
+      'http' => [
+        'method' => 'PUT',
+        'header' => 'Content-type: application/json'          ."\r\n".
+                    'Authorization: Bearer '.$qiitaAccessToken."\r\n",
+      ]
+    ];
+    $context = stream_context_create($context);
+    $response = file_get_contents($url, false, $context);
+    if ($response === false) {
+        error_log('Qiita API response error');
+        exit;
+    }
 }
 
-error_log('Curl error: '.curl_error($ch));
+error_log('Success qiita stock');
